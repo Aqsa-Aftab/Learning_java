0375459136cda5c600d48aaa2ad1e94ec8a1b55c
Aqsa-Aftab <86191505+Aqsa-Aftab@users.noreply.github.com>
2021-06-27 14:13:00 +0530

    Time: 1 ms (80.60%), Space: 39.2 MB (47.48%) - LeetHub

---
+++ two-sum/two-sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+       
+
+		int[] result =new int[2];
+		
+		
+		Map<Integer,Integer > map = new HashMap<Integer, Integer>();
+		
+		
+		
+		
+	
+		for(int i = 0; i<nums.length; i++) {
+			
+			if(map.containsKey(target-nums[i])) {
+				
+				result[0] = map.get(target-nums[i]);
+				result[1] = i;
+				break;
+			}
+			else
+			{
+				map.put(nums[i], i);
+				
+				
+			}
+				
+			
+		}
+        return result;
+    }
+}
