27015b94fb8c0f927ed4ac279be99bef8a03741b
Aqsa-Aftab <86191505+Aqsa-Aftab@users.noreply.github.com>
2021-06-28 07:38:36 +0530

    Time: 1 ms (31.04%), Space: 40.4 MB (94.75%) - LeetHub

---
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+     
+        int k = 0;
+        
+        
+        for(int i = 1; i<nums.length; i++){
+            
+            if(nums[i]!= nums[k]){
+                k++;
+                nums[k] = nums[i];
+                
+            }
+            
+        }
+        
+        return k+1;
+    }
+}
